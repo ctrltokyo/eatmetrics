3e00de791943f9c18a2bfbf2497e7e4e7ba460fd
DragonStuff <me@0xdragon.com>
2020-04-16 11:07:18 +0900

    feature(em): add scheduled work genserver template

---
+++ app/em.ex
@@ -0,0 +1,35 @@
+defmodule App.Em do
+  @moduledoc false
+  
+
+
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    # Schedule work to be performed on start
+    schedule_work()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:work, state) do
+    # Do the desired work here
+    # ...
+
+    # Reschedule once more
+    schedule_work()
+
+    {:noreply, state}
+  end
+
+  defp schedule_work do
+    # In 2 hours
+    Process.send_after(self(), :work, 2 * 60 * 60 * 1000)
+  end
+end
