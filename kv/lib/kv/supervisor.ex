4d8b56a320ba1fce0c30cfc226b32cc20e210678
DragonStuff <me@0xdragon.com>
2020-04-25 20:17:23 +0900

    feature(registry): make Supervisor start on run and fix kv test

---
+++ kv/lib/kv/supervisor.ex
@@ -0,0 +1,20 @@
+defmodule KV.Supervisor do
+  @moduledoc false
+  
+
+
+  use Supervisor
+
+  def start_link(arg) do
+    Supervisor.start_link(__MODULE__, :ok, arg)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      {KV.Registry, name: KV.Registry},
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
